b404837ee9ba9ef098e4d7631ecb0d9b4c94a061
tlf <1147664049@qq.com>
2018-12-20 16:34:03 +0800

    分治排序算法

--- app/src/test/java/com/tall/suanfa/FenZhi.java
+++ app/src/test/java/com/tall/suanfa/FenZhi.java
@@ -2,6 +2,8 @@ package com.tall.suanfa;
 
 import org.junit.Test;
 
+import java.util.TreeMap;
+
 /**
  * 分治法
  * <p>
@@ -12,9 +14,14 @@ public class FenZhi {
 
     @Test
     public void test() throws Exception {
-        int[] array = new int[]{1, 2, 4, 5, 6, 7, 8, 9};
-        int i = binarySearch(array, 9);
-        System.out.println(i);
+        int[] array = new int[]{3, 4, 4, 44, 32, 21, 77, 88, 21};
+        // int i = binarySearch(array, 9);
+        //System.out.println(i);
+        quickSort(array, 0, 8);
+        for (int i : array) {
+            System.out.println(i);
+
+        }
     }
 
 
@@ -40,6 +47,114 @@ public class FenZhi {
         return -1;
 
     }
+
+    /**
+     * 快速排序
+     */
+    public static void quickSort(int[] array, int begin, int end) {
+        if (begin >= end) return;
+        int x = array[begin];
+        int low = begin;
+        int high = end;
+
+        //由于会从两头取数据，需要一个方向
+        boolean direction = true;
+        L:
+        while (low < high) {
+            if (direction) {//从右往左找
+                for (int i = high; i > low; i--) {
+                    if (array[i] <= x) {
+                        array[low++] = array[i];
+                        high = i;
+                        direction = !direction;
+                        continue L;
+
+                    }
+
+                }
+                //假如全程没有进入上方的if的话，指针重合
+                high = low;
+
+            } else {//从左往右找
+                for (int i = low; i < high; i++) {
+                    if (array[i] >= x) {
+                        array[high--] = array[i];
+                        low = i;
+                        direction = !direction;
+                        continue L;
+
+                    }
+
+                }
+                //假如全程没有进入上方的if的话，指针重合
+                low = high;
+
+            }
+
+        }
+
+        array[low] = x;
+        quickSort(array, begin, low - 1);
+        quickSort(array, low + 1, end);
+
+
+    }
+
+
+    /**
+     * 归并排序
+     */
+    public static void mergeSort(int array[],int left,int right){
+        if(left==right){
+            return;
+        }else{
+            int mid=(left+right)/2;
+            mergeSort(array,left,mid);
+            mergeSort(array,mid+1,right);
+            merge(array,left,mid+1,right);
+        }
+    }
+
+    /**
+     * 归并算法
+     * 1  2  5  9 === 3  4  10  11
+     */
+    public static void merge(int[] array,int left,int mid,int right){
+        int leftSize=mid-left;
+        int rightSize=right-mid+1;
+        //生成数组
+        int[] leftArray=new int[leftSize];
+        int[] rightArray=new int[rightSize];
+        //填充数据
+        for(int i=left;i<mid;i++){
+            leftArray[i-left]=array[i];
+        }
+        for(int i=mid;i<=right;i++){
+            rightArray[i-mid]=array[i];
+        }
+        //合并
+        int i=0;
+        int j=0;
+        int k=left;
+        while(i<leftSize && j<rightSize){
+            if(leftArray[i]<rightArray[j]){
+                array[k]=leftArray[i];
+                k++;i++;
+            }else{
+                array[k]=rightArray[j];
+                k++;j++;
+            }
+        }
+        while(i<leftSize){
+            array[k]=leftArray[i];
+            k++;i++;
+        }
+        while(j<rightSize){
+            array[k]=rightArray[j];
+            k++;j++;
+        }
+    }
+
 
 
 
